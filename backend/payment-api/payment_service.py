3b45f0be9565855d0eae8fc855dcb8fd9f8d736f
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-14 17:09:49 -0300

    Update payment_service.py

--- backend/payment-api/payment_service.py
+++ backend/payment-api/payment_service.py
@@ -1,1 +1,9 @@
+def process_payment(data):
+    if 'amount' not in data:
+        raise ValueError("Parâmetro 'amount' é obrigatório")
+    # Lógica fictícia
+    return {
+        "status": "sucesso",
+        "valor_processado": data["amount"]
+    }
 
